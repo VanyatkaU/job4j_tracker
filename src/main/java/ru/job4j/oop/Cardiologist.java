dc84bd8c6ddcf2c9eb68014880f64b3789e98aae
VanyatkaU <ivanKa49@ya.ru>
2021-09-08 21:04:50 +0300

    3. Реализация профессий в коде [#6837]

---
+++ src/main/java/ru/job4j/oop/Cardiologist.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class Cardiologist extends Doctor {
+    String experience;
+    String numberOfOperations;
+    String salary;
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public String getNumberOfOperations() {
+        return numberOfOperations;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Metallurgist.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class Metallurgist extends Engineer {
+    String direction;
+    String category;
+    String salary;
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+}

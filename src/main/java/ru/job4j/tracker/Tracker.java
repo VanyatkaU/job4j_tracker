6d97ab27b9f626897d23e344783bd94130ac7184
VanyatkaU <ivanKa49@ya.ru>
2021-09-18 17:03:10 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -52,4 +52,13 @@ public class Tracker {
         }
         return index != -1;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size--] = null;
+        }
+        return index != -1;
+    }
 }

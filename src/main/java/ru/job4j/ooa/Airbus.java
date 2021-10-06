2528fcaa16445ab59161890aba7a3cf523ab29b9
VanyatkaU <ivanKa49@ya.ru>
2021-10-06 16:01:37 +0300

    2. final [#334073 #212534]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,8 +23,8 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        System.out.println("Количество двигателей равно: "
-                + (name.equals("A380") ? 4 : COUNT_ENGINE));
+        int count = "A380".equals(name) ? 4 : COUNT_ENGINE;
+        System.out.println("Количество двигателей равно: " + count);
     }
 
     @Override

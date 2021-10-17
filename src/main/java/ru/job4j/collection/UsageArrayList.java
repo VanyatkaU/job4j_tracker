0f8ff7afba01c368b2e9deef6a4907c02bd69a88
VanyatkaU <ivanKa49@ya.ru>
2021-10-17 22:11:56 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094 #217170]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -8,7 +8,7 @@ public class UsageArrayList {
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
-        for (Object vaule : names) {
+        for (String vaule : names) {
             System.out.println(vaule);
         }
     }

4e5e6c8683abd40352c9bf0dad709f7b91df5897
VanyatkaU <ivanKa49@ya.ru>
2021-12-01 15:32:08 +0300

    0. Stream API [#58166 #234574]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -1,29 +1,13 @@
 package ru.job4j.stream;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamUsage {
-    public static class Task {
-        private final String name;
-        private final long spent;
-
-        public Task(String name, long spent) {
-            this.name = name;
-            this.spent = spent;
-        }
-    }
-
     public static void main(String[] args) {
-        List<Task> tasks = List.of(
-                new Task("Bug #1", 10),
-                new Task("Task #2", 20),
-                new Task("Bug #3", 40)
-        );
-        tasks.stream().
-                filter(task -> task.name.contains("Bug"))
-                .filter(task -> task.spent > 30)
-                .map(task -> task.name + " " + task.spent)
-                .forEach(System.out::println);
+        List<Integer> num = new ArrayList<>(List.of(1, 2, -3, -4, 5, -6, 7, -8, 9));
+        List<Integer> nums = num.stream().
+                filter(n -> (n > 0)).collect(Collectors.toList());
+        nums.forEach(System.out::println);
     }
 }
-

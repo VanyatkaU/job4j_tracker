a94c4201efe0ad5d02f230ca13a5f98eda3dfd8c
VanyatkaU <ivanKa49@ya.ru>
2021-12-02 12:55:44 +0300

    0. Stream API [#58166 #234574]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class StreamUsage {
     public static void main(String[] args) {
-        List<Integer> num = new ArrayList<>(List.of(1, 2, -3, -4, 5, -6, 7, -8, 9));
+        List<Integer> num = List.of(1, 2, -3, -4, 5, -6, 7, -8, 9);
         List<Integer> nums = num.stream().
                 filter(n -> (n > 0)).collect(Collectors.toList());
         nums.forEach(System.out::println);

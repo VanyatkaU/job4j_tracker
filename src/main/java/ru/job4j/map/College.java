d73696353d83ea93e502e9c2e257d8a770845134
VanyatkaU <ivanKa49@ya.ru>
2021-12-07 22:34:27 +0300

    3. Optional в банковских переводах. [#214711]

---
+++ src/main/java/ru/job4j/map/College.java
@@ -0,0 +1,54 @@
+package ru.job4j.map;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class College {
+    private final Map<Student, Set<Subject>> students;
+
+    public College(Map<Student, Set<Subject>> students) {
+        this.students = students;
+    }
+
+    public Optional<Student> findByAccount(String account) {
+        Optional<Student> rsl = Optional.empty();
+        for (Student s : students.keySet()) {
+            if (s.getAccount()
+                    .equals(account)) {
+                rsl = Optional.of(s);
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Subject> rsl = Optional.empty();
+        Optional<Student> a = findByAccount(account);
+        if (a.isPresent()) {
+            Set<Subject> subjects = students.get(a.get());
+            for (Subject s : subjects) {
+                if (name.equals(s.getName())) {
+                    rsl = Optional.of(s);
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Map<Student, Set<Subject>> students = Map.of(new Student("Student", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                )
+        );
+        College college = new College(students);
+        Optional<Student> student = college.findByAccount("000001");
+        System.out.println("Найденный студент: " + student);
+        Optional<Subject> english = college.findBySubjectName("000001", "English");
+        System.out.println("Оценка по найденному предмету: " + english.get().getScore());
+    }
+}

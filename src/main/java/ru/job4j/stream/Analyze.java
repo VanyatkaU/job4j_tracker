12c8f34fcb16ddbac367288b804e50cc9bc72422
VanyatkaU <ivanKa49@ya.ru>
2021-12-10 14:22:32 +0300

    2. Аттестация [#49777]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -28,7 +28,8 @@ public class Analyze {
     }
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
-        return stream.flatMap(pupil -> pupil.getSubjects()
+        return stream
+                .flatMap(pupil -> pupil.getSubjects()
                         .stream())
                 .collect(Collectors.groupingBy(Subject::getName,
                                 Collectors.averagingDouble(Subject::getScore)))
@@ -44,20 +45,20 @@ public class Analyze {
                         .stream()
                         .mapToInt(Subject::getScore)
                         .sum()))
-                .max(Comparator.comparing(Tuple::hashCode))
+                .max(Comparator.comparingDouble(Tuple::getScore))
                 .orElse(null);
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
         return stream
-                .flatMap(s -> Stream.of(s.getSubjects()))
-                .flatMap(List::stream)
+                .flatMap(pupil -> pupil.getSubjects()
+                        .stream())
                 .collect(Collectors.groupingBy(Subject::getName,
                         Collectors.summingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
                 .map(pupil -> new Tuple(pupil.getKey(), pupil.getValue()))
-                .max(Comparator.comparing(Tuple::hashCode))
+                .max(Comparator.comparingDouble(Tuple::getScore))
                 .orElse(null);
     }
 }
--- src/main/java/ru/job4j/stream/Subject.java
+++ src/main/java/ru/job4j/stream/Subject.java
@@ -1,5 +1,7 @@
 package ru.job4j.stream;
 
+import java.util.Objects;
+
 public class Subject {
     private final String name;
     private final int score;
@@ -15,5 +17,31 @@ public class Subject {
 
     public int getScore() {
         return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Subject subject = (Subject) o;
+        return score == subject.score
+                && Objects.equals(name, subject.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{"
+                + "name='" + name + '\''
+                + ", score=" + score
+                + '}';
     }
 }

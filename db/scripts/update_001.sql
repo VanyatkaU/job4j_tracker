a6cd3f13ce38b08c2fbce0403d32db4c159ac957
VanyatkaU <ivanKa49@ya.ru>
2022-08-20 23:43:06 +0300

    1. Liquibase. Установка. [#504892]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,5 +1,5 @@
-create table if not exists items(
-    id serial primary key,
-    name text,
-    created timestamp
+create table if not exists items (
+   id serial primary key,
+   name text,
+   created timestamp
 );
